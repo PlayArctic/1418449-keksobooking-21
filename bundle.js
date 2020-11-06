93ea0fcd4cdc6740155d8a75fa82f5dd8a314d09
Dmitriy Palchuk <dmitry.palchuk@vrpconsulting.com>
2020-11-06 14:14:24 +0300

    Исправлена проблема с загрузкой изображений

--- bundle.js
+++ bundle.js
@@ -362,55 +362,49 @@ window.handlers = {
 
 let FILE_TYPES = [`jpg`, `jpeg`, `png`, `gif`];
 
-let AVATAR_TARGETS = {
-  avatar: {
+let AVATAR_TARGETS = [
+  {
     input: document.querySelector(`.ad-form-header__input`),
     output: document.querySelector(`.ad-form-header__preview img`)
   },
-  photo: {
+  {
     input: document.querySelector(`.ad-form__input`),
     output: document.querySelector(`.ad-form__photo`)
   }
-};
+];
 
 
-// AVATAR_TARGETS.avatar.input.addEventListener(`click`, () => console.log(`Succsess`));
+// AVATAR_TARGETS.avatar.input.addEventListener(`click`, () => console.log(`Succsess`)); // !работает
 
 let setAvatar = function (pictureTargets) {
-  console.log(`0`, pictureTargets)
-  for (let elem in pictureTargets) {
-    console.log(`1`, pictureTargets.elem)
-    console.log(`2`, pictureTargets)
-    console.log(`3`, elem)
-    // if (pictureTargets.hasOwnProperty(elem)) {
-    //   (pictureTargets.elem.input).addEventListener(`change`, function () {
-    //     let file = (pictureTargets.elem.input).files[0];
-    //     let fileName = file.name.toLowerCase();
+  for (let elem of pictureTargets) {
+    elem.input.addEventListener(`change`, function () {
+      let file = elem.input.files[0];
+      let fileName = file.name.toLowerCase();
 
-    //     let matches = FILE_TYPES.some(function (ending) {
-    //       return fileName.endsWith(ending);
-    //     });
+      let matches = FILE_TYPES.some(function (ending) {
+        return fileName.endsWith(ending);
+      });
 
-    //     if (matches) {
-    //       let reader = new FileReader();
+      if (matches) {
+        let reader = new FileReader();
 
-    //       reader.readAsDataURL(file);
+        reader.readAsDataURL(file);
 
-    //       reader.addEventListener(`load`, function () {
-    //         if ((pictureTargets.elem.output).tagName.toLowerCase() === `img`) {
-    //           elem.output.src = reader.result;
-    //         } else {
-    //           let newElement = document.createElement(`img`);
-    //           newElement.src = reader.result;
+        reader.addEventListener(`load`, function () {
+          if (elem.output.tagName.toLowerCase() === `img`) {
+            elem.output.src = reader.result;
+          } else {
+            let newElement = document.createElement(`img`);
+            newElement.src = reader.result;
 
-    //           newElement.setAttribute(`style`, `border-radius: 5px; width: 100%; height: 100%;`);
+            newElement.setAttribute(`style`, `border-radius: 5px; width: 100%; height: 100%;`);
 
-    //           (pictureTargets.elem.output).appendChild(newElement);
-    //         }
-    //       });
-    //     }
-    //   });
-    // }
+            elem.output.appendChild(newElement);
+          }
+        });
+      }
+    });
   }
 };
 
